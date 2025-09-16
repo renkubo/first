0a6aa7b24e5cc9d5b111d32f0806f9a04d50a372
sigur <sigur@DESKTOP-DURHIET>
2025-09-16 16:41:32 +0900

    自己紹介

---
+++ HelloWorld_KuboRen/src/sample/SelfIntroduction.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		String name = "久保 蓮";
+		int age = 24;
+		String hobby = "映画鑑賞";
+		
+		/*
+		* 自己紹介文を出力する処理
+		*/
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
